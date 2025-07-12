9c5312f2c2018261e0dbe8ac90ac7c4271bb08f7
Akash Patel <akashpatel260603@gmail.com>
2025-07-12 20:32:21 +0530

    done-7-time

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,21 @@ import './../styles/App.css';
 const App = () => {
  const[name , setName] = useState("")
   return (
-    <div>
+    <div className="app-container">
         {/* Do not remove the main div */}
         <p>Enter your name:</p>
-        <input type="text" onChange={(e)=>{
+        <input
+         type="text"
+          onChange={(e)=>{
           setName(e.target.value)
-      }}name="greet" id="greeding" value= {name}  className=" " placeholder="Enter your name"/>
-      {name && <p>Hello,{name}!</p>}
+      }}
+      name="greet" 
+      id="greeding" 
+      value= {name}  
+      className="input-field" 
+      placeholder="Enter your name"
+      />
+      {name && <p className="greeting-text">Hello,{name}!</p>}
     </div>
   )
 }
